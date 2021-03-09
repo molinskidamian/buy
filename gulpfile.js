3acb3e6e7bf636bc3cd2e6b684a0eebc4d65a32b
Damian Molinski <tedeer@gmail.com>
2021-03-09 22:34:13 +0100

    basic directory structure has been created

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+
+gulp.task('buildcss', () => gulp
+  .src('./dev-assets/style.scss')
+  .pipe(gulpSass())
+  .pipe(gulp.dest('./prod-assets')));
+
+gulp.task('watch', () => {
+  gulp.watch('./dev-assets/**/*.scss', gulp.series('buildcss'));
+});
